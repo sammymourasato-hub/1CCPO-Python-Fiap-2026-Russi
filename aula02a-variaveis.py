8bde40926a3962f0f35c8f54c68fe8ee83ecaec9
sammymourasato-hub <sammymourasato@gmail.com>
2026-03-19 20:57:14 -0300

    aula 2 variaveis e operadores

---
+++ aula02a-variaveis.py
@@ -0,0 +1,37 @@
+print("olá mundo!")
+#Operação
+print(7 + 4)
+
+#String - mostrar na tela ("")
+print("7 + 4")
+
+print("7" + "4") # Concatenação de strings (74)
+"""
+Comentarios de 
+Multiplas 
+Linhas
+
+"""
+
+# Variáveis
+nome = "Alexandre"
+print(nome) #STRING (str)
+idade = 26 #INTEIRO (int)
+peso = 72.2 #float
+
+print (nome, idade, peso)
+print (f"oi, {nome}!!!")
+
+#INPUT - SIMULAÇÃO DE FORMS NO CMD
+nome = input("Digite o seu nome: ")
+idade = int(input("Digite a idade: "))
+peso = float(input ("Digite o peso: "))
+
+print(nome, idade, peso)
+print(idade + 1)
+
+#"_" para dar espaço
+ano_nascimento = 1999
+ano_atuaç = 2026
+idade = ano_atual - ano_nascimento
+print(f"Sua idade é: {idade}")
