d4f8370bd07417c5c05c3e3edba815944ff64fbd
sammymourasato-hub <sammymourasato@gmail.com>
2026-03-19 20:57:29 -0300

    aula 2 variaveis e operadores

---
+++ Exercicios-aula2.py
@@ -0,0 +1,15 @@
+#Desafio 01
+nome = input("Digite seu nome: ")
+print(f"Seja bem-vindo, {nome}")
+
+#Desafio 2
+dia_de_Nasc = input("Digite o dia em que você nasceu: ")
+mes_de_Nasc = input("Dgite seu mês de nascimento: ")
+ano_de_Nasc = input("Digite seu  ano de nascimento: ")
+print(f"{dia_de_Nasc}/{mes_de_Nasc}/{ano_de_Nasc}")
+
+#Desafio 3
+num1 = int(input("Digite um número: "))
+num2 = int(input("Digite outro número"))
+num3 = num1 + num2
+print(f"Resultado: {num3}")
